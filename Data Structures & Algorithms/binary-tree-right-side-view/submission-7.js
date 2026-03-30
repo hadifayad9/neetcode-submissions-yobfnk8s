952110901990163b841dc23c16f779bd32035b86
hadifayad9 <69330947+hadifayad9@users.noreply.github.com>
2026-03-30 16:15:36 -0400

    Add: binary-tree-right-side-view - submission-7

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-7.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+   rightSideView(root) {
+    if (!root) {
+      return [];
+    }
+    let binaryQueue = [root];
+    let result = [];
+    while (binaryQueue.length !== 0) {
+      const binaryQueueLength = binaryQueue.length;
+      for (let i = 0; i < binaryQueueLength; i++) {
+        const node = binaryQueue.shift();
+        if (i === binaryQueueLength - 1) {
+          result.push(node.val);
+        }
+        if (node.left) {
+          binaryQueue.push(node.left);
+        }
+        if (node.right) {
+          binaryQueue.push(node.right);
+        }
+      }
+    }
+    return result;
+  }
+}
