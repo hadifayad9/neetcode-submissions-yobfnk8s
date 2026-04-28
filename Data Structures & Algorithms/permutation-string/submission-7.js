4a1ad591c5bb366c1817efa143a568f79265ff56
hadifayad9 <69330947+hadifayad9@users.noreply.github.com>
2026-04-28 19:51:40 -0400

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+  checkInclusion(s1, s2) {
+    let s1Array = new Array(26).fill(0);
+    let s2Array = new Array(26).fill(0);
+    for (let i = 0; i < s1.length; i++) {
+      s1Array[s1[i].charCodeAt(0) - "a".charCodeAt(0)]++;
+    }
+    let left = 0;
+    for (let i = 0; i < s2.length; i++) {
+      let isMatched = true;
+      s2Array[s2[i].charCodeAt(0) - "a".charCodeAt(0)]++;
+      if (i - left + 1 > s1.length) {
+        s2Array[s2[left].charCodeAt(0) - "a".charCodeAt(0)]--;
+        left++;
+      }
+      if (i - left + 1 === s1.length) {
+        for (let j = 0; j < s1Array.length; j++) {
+          if (s1Array[j] !== s2Array[j]) {
+            isMatched = false;
+          }
+        }
+        if (isMatched) {
+          return true;
+        }
+      }
+    }
+      return false;
+  }
+}
