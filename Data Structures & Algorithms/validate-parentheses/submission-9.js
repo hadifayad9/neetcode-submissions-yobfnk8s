d398714fae90aaeaf4a470cf44d5f6954549b0a2
hadifayad9 <69330947+hadifayad9@users.noreply.github.com>
2026-04-29 15:38:11 -0400

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+  isValid(s) {
+    if (s.length % 2 === 1) {
+      return false;
+    }
+    let obj = {
+      "}": "{",
+      "]": "[",
+      ")": "(",
+    };
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+      if (obj.hasOwnProperty(s[i])) {
+        if (stack.length > 0 && obj[s[i]] === stack[stack.length - 1]) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      } else {
+        stack.push(s[i]);
+      }
+    }
+    return stack.length === 0 ? true : false;
+  }
+}
