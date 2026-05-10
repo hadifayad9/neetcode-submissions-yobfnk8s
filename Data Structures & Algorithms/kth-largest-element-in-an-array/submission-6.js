96519eab41adc4e1f01ae768eaa780485cb331d7
hadifayad9 <69330947+hadifayad9@users.noreply.github.com>
2026-05-09 20:14:14 -0400

    Add: kth-largest-element-in-an-array - submission-6

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-6.js
@@ -0,0 +1,130 @@
+class MinHeap {
+  constructor() {
+    this.data = [];
+  }
+
+  size() {
+    return this.data.length;
+  }
+
+  peek() {
+    return this.data.length ? this.data[0] : undefined;
+  }
+
+  push(val) {
+    this.data.push(val);
+    let i = this.data.length - 1;
+    while (i > 0) {
+      let parent = this.getParentIndex(i);
+      if (this.data[i] < this.data[parent]) {
+        this.swap(i, parent);
+        i = parent;
+      } else {
+        break;
+      }
+    }
+  }
+  pop() {
+    if (this.data.length === 0) return undefined;
+    if (this.data.length === 1) return this.data.pop();
+
+    this.swap(0, this.data.length - 1);
+    const removed = this.data.pop();
+
+    let i = 0;
+
+    while (true) {
+      const left = this.getLeftChildIndex(i);
+      const right = this.getRightChildIndex(i);
+      let smallest = i;
+
+      if (left < this.data.length && this.data[left] < this.data[smallest]) {
+        smallest = left;
+      }
+
+      if (right < this.data.length && this.data[right] < this.data[smallest]) {
+        smallest = right;
+      }
+      if (smallest === i) break;
+      this.swap(i, smallest);
+      i = smallest;
+    }
+    return removed;
+  }
+
+  getParentIndex(i) {
+    return Math.floor((i - 1) / 2);
+  }
+
+  getLeftChildIndex(i) {
+    return 2 * i + 1;
+  }
+
+  getRightChildIndex(i) {
+    return 2 * i + 2;
+  }
+
+  swap(i, j) {
+    const temp = this.data[i];
+    this.data[i] = this.data[j];
+    this.data[j] = temp;
+  }
+}
+
+/**
+ * Last Stone Weight
+Easy Topics Company Tags
+Hints
+
+You are given an array of integers stones where stones[i] represents the weight of the ith stone.
+
+We want to run a simulation on the stones as follows:
+
+    At each step we choose the two heaviest stones, with weight x and y and smash them togethers
+    If x == y, both stones are destroyed
+    If x < y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+
+Continue the simulation until there is no more than one stone remaining.
+
+Return the weight of the last remaining stone or return 0 if none remain.
+
+Example 1:
+
+Input: stones = [2,3,6,2,4]
+
+Output: 1
+
+Explanation:
+We smash 6 and 4 and are left with a 2, so the array becomes [2,3,2,2].
+We smash 3 and 2 and are left with a 1, so the array becomes [1,2,2].
+We smash 2 and 2, so the array becomes [1].
+
+Example 2:
+
+Input: stones = [1,2]
+
+Output: 1
+
+Constraints:
+
+    1 <= stones.length <= 20
+    1 <= stones[i] <= 100
+
+ */
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @param {number} k
+   * @return {number}
+   */
+  findKthLargest(nums, k) {
+    let minHeap = new MinHeap();
+    nums.forEach((element) => {
+      minHeap.push(element);
+      if (minHeap.size() > k) {
+        minHeap.pop();
+      }
+    });
+    return minHeap.peek();
+  }
+}
