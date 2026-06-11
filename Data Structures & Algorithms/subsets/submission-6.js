9b4f2360777a0bd2d8fa5e8b5bb16dcb01a1d814
hadifayad9 <69330947+hadifayad9@users.noreply.github.com>
2026-06-10 20:50:15 -0400

    Add: subsets - submission-6

---
+++ Data Structures & Algorithms/subsets/submission-6.js
@@ -0,0 +1,20 @@
+class Solution {
+  subsets(nums) {
+    let subset = [];
+    let result = [];
+    function dfs(i) {
+      if (i >= nums.length) {
+        result.push([...subset]);
+        return;
+      }
+      subset.push(nums[i]);
+      dfs(i + 1);
+
+      subset.pop();
+
+      dfs(i + 1);
+    }
+    dfs(0);
+    return result;
+  }
+}
