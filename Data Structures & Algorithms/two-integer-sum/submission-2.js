ee9298f0d1e8dfb71fe5088d60ea6c8cf30d5e8f
hadifayad9 <69330947+hadifayad9@users.noreply.github.com>
2026-06-17 20:48:18 -0400

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+  twoSum(nums, target) {
+    let obj = {};
+    for (let i = 0; i < nums.length; i++) {
+      if (obj.hasOwnProperty(target - nums[i])) {
+        return [obj[target - nums[i]], i];
+      } else {
+        obj[nums[i]] = i;
+      }
+    }
+  }
+}
