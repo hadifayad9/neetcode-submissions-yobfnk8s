781a215c340527be3a9084a6336c23b01c465dac
hadifayad9 <69330947+hadifayad9@users.noreply.github.com>
2026-09-09 18:20:53 -0400

    Add: generate-parentheses - submission-3

---
+++ Data Structures & Algorithms/generate-parentheses/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+  generateParenthesis(n) {
+    let path = [];
+    let result = [];
+    function dfs(opening, closing) {
+      if (opening === 0 && closing === 0 && path.length === n * 2) {
+        result.push([...path].join(''));
+        return;
+      }
+      if (opening > 0) {
+        path.push('(');
+        dfs(opening - 1, closing);
+        path.pop();
+      }
+      if (closing > opening) {
+        path.push(')');
+        dfs(opening, closing - 1);
+        path.pop();
+      }
+    }
+    dfs(n, n);
+    return result;
+  }
+}
